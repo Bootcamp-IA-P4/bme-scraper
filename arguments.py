d663d1926be766b54d558aa0ee29875f62ec5a56
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-03-24 18:05:35 +0100

    fix(arguments.py) : version arg taken out of scrape_group

--- arguments.py
+++ arguments.py
@@ -10,10 +10,12 @@ def argument_parser():
     parser.add_argument('-V', '--verbose', help="Show detailed output for each scrape.", action='store_true',default=False)
     parser.add_argument('-w', '--wait_time', help="Time to wait between requests.", type=int, default=wait_time)
     parser.add_argument('-b', '--browser', help="Show driver browser.", type=bool, default=browser_display)
+    
     #Main group exclusive 
     main_option = parser.add_mutually_exclusive_group(required=True)
     main_option.add_argument('-scr','--scrape', help="Scrape data from the website.", action='store_true',default=False)
     main_option.add_argument('-db','--database', help="Manage db options.", action='store_true',default=False)
+    main_option.add_argument('-v', '--version', help="Show program version.", action='version', version="BME Scraper v1.0.0_beta")
     #DB group exclusive
     database = parser.add_mutually_exclusive_group(required=False)
     database.add_argument('-du', '--dump', help="Dump database to file.", action='store_true',default=False)
@@ -21,7 +23,6 @@ def argument_parser():
     database.add_argument('-delbd', '--delete_db', help="Delete all rows from all tables.", action='store_true',default=False)
     #Scrape group exclusive
     scrape_group = parser.add_mutually_exclusive_group(required=False)
-    scrape_group.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
     scrape_group.add_argument('-a','--all', action='store_true', help='Scrape all data')
     scrape_group.add_argument('-c','--companies', action='store_true', help='Scrape companies')
     scrape_group.add_argument('-s','--stock_values', action='store_true', help='Scrape stock values')
